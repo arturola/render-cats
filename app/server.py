73b12a60d76d28c7356386870112086e67324c28
arturola <37004011+arturola@users.noreply.github.com>
2019-12-01 00:45:41 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1t-jQvRlvzlTOBTVCRqs7Du9mTMNqJhCR'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1mKNJQtSAzEOJYvsFA0DUr_5jr2entqkn'
 export_file_name = 'export.pkl'
 
 classes = ['almendras', 'avellanas', 'nueces']
